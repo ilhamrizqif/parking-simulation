418dcf6d612a8ef45ad42668bf357f4ffe8f58ad
ilhamrizqif <ir.fadillah27@gmail.com>
2022-11-18 03:56:44 -0500

    Update Source Code

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+Route::get('/checkin', function () {
+    return view('checkin');
+});
+Route::get('/checkout', function () {
+    return view('checkin');
+});
+Route::get('/report', function () {
+    return view('dashboard');
+});
+
